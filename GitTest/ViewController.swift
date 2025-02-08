6b0f84c6045a4593feb5c39dde099b42e556b27d
ppx <3375552334@qq.com>
2025-02-08 15:12:37 +0800

    1.0.0 增加按钮和文本

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -1,19 +1,42 @@
-//
-//  ViewController.swift
-//  GitTest
-//
-//  Created by ppx on 2025/2/8.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // 创建 UILabel
+        let label = UILabel()
+        label.text = "111"
+        label.translatesAutoresizingMaskIntoConstraints = false // 关闭自动转换
+        view.addSubview(label)
+
+        // 使用原生自动布局为 UILabel 进行布局
+        NSLayoutConstraint.activate([
+            label.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            label.widthAnchor.constraint(equalToConstant: 20),
+            label.heightAnchor.constraint(equalToConstant: 20)
+        ])
+
+        // 创建 UIButton
+        let button = UIButton(type: .system)
+        button.setTitle("111", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false // 关闭自动转换
+        view.addSubview(button)
+
+        // 使用原生自动布局为 UIButton 进行布局
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 100),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
-
+    // 按钮点击事件处理方法
+    @objc func buttonTapped() {
+        print("按钮被点击了！")
+    }
 }
-
